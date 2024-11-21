d811d267a088a19f60b7043960982c7c0e157657
ilovemodok <ilovemodok@hotmail.com>
2024-11-21 21:19:15 +0900

    Manages to remove all Astro stuff and keep just the Vite Stuff

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-import "./style.css";
+// import "./style.css";
 
 //image example for Vite
 import profileImage from "./src/images/homepage/gw-profile.png?format=webp";
 document.getElementById("profile-image").src = profileImage;
-
-setupCounter(document.querySelector("#counter"));
