3cb85cc87565595175d3986c89ffbce6abadd7c0
ilovemodok <ilovemodok@hotmail.com>
2024-11-23 19:27:04 +0900

    Connected CSS files

--- main.js
+++ main.js
@@ -1,32 +1,14 @@
 //CSS
+import "@/styles/components.css";
+import "@/styles/layout.css";
 import "@/styles/reset.css";
+import "@/styles/typography.css";
+import "@/styles/variables.css";
 
 //image example for Vite
-import profileImage from "@/images/homepage/gw-profile.png?format=webp";
+// import profileImage from "@/images/homepage/gw-profile.png?format=webp";
+
+// Rebel Leaf Logo
 import rebelleaflogo from "@/images/global/rl-logo.svg";
-
-document.getElementById("profile-image").src = profileImage;
-document.getElementById("rebelleaf-logo").src = rebelleaflogo;
-
 const rebelleafLogo = document.querySelector("#rebelleaf-logo");
-rebelleafLogo.addEventListener("click", () => {
-  alert("Sup!");
-});
-
-const btn = document.querySelector("#btn");
-
-function alertFunction() {
-  alert("Bugger off!");
-}
-
-btn.addEventListener("click", function (e) {
-  e.target.style.background = "blue";
-});
-
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    alert(button.id);
-  });
-});
+rebelleafLogo.src = rebelleaflogo;
